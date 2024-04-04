d5277f649e88cf077faa258859bf4a91a69db765
Titusz Papp <hfmyrn@inf.elte.hu>
2024-04-04 11:18:05 +0200

    Level.java-ba a filereadert megírtam, level.txt file az alap szintet tartalmazza. 1-Fal 2-Box bármi más-üres

--- bomberman/src/Game/Level.java
+++ bomberman/src/Game/Level.java
@@ -1,9 +1,51 @@
 package Game;
 
 import java.awt.image.BufferedImage;
+import java.awt.Image;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.awt.Graphics;
 
 public class Level {
     private BufferedReader br = new BufferedReader(new FileReader(levelPath));
     private ArrayList<Sprite> grid;
     private ArrayList<Bomb> bombs;
-}
+
+    public Level(String levelPath) throws IOException {
+        fileToLevel(levelPath);
+    }
+
+    public void loadLevel(String levelPath) throws FileNotFoundException, IOException {
+        BufferedReader br = new BufferedReader(new FileReader(levelPath));
+        int block_width = 50;
+        int block_height = 50;
+        int y = 0;
+        String line;
+        while ((line = br.readLine()) != null){
+            int x = 0;
+            for (char blockType : line.toCharArray()) {
+                if(blockType == '1'){
+                    Image image = new ImageIcon("data/images/wall.jpg").getImage();
+                    grid.add(new Wall(x*block_width, y*block_height, ,image, block_height,block_width));
+                }
+                else if(blockType == '2'){
+                    Image image = new ImageIcon("data/images/box.png").getImage();
+                    grid.add(new Box(x*50, y*50, block_width,block_height,image));
+                }x++;
+            }
+            y++;
+        }
+    }
+
+
+    public void draw(Graphics g) {
+        for (Sprite sprite : grid) {
+            sprite.draw(g);
+        }
+        for (Bomb bomb : bombs) {
+            bomb.draw(g);
+        }
+    }
